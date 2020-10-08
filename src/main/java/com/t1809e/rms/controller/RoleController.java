c9a026dd54027098afdf74c25f74216d635280da
manhtrangtn <manhtrangtn@gmail.com>
2020-10-09 00:11:31 +0700

    Refactor some codes and update latest codes

--- src/main/java/com/t1809e/rms/controller/RoleController.java
+++ src/main/java/com/t1809e/rms/controller/RoleController.java
@@ -1,7 +1,10 @@
 package com.t1809e.rms.controller;
 
+import com.t1809e.rms.utility.constance.Path;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = Path.PATH_ROLE)
 public class RoleController {
 }
---
+++ src/main/java/com/t1809e/rms/utility/constance/Path.java
@@ -0,0 +1,15 @@
+package com.t1809e.rms.utility.constance;
+
+public interface Path {
+    //base path
+    String PATH_USER = "/user";
+    String PATH_MENU = "/menu";
+    String PATH_ROLE = "/role";
+
+    //path
+    String PATH_CREATE = "/create";
+    String PATH_DELETE = "/delete";
+    String PATH_UPDATE = "update";
+    String PATH_GET = "/get";
+    String PATH_GETs = "/get-list";
+}
